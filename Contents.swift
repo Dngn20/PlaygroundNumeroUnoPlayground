59da198af77626fbc943888fa6bfb722a226f0ab
Dngn20 <dngu9879@740s-pl-998.local>
2016-09-15 13:08:07 -0600

    Test run of swift and learning to program with swift

---
+++ Contents.swift
@@ -0,0 +1,18 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+var str = "Hello, playground"
+var numbersKey = 545
+print(numbersKey)
+
+var green, red, blue: Double
+print("green is a cool color")
+
+let face = "😅"; print(face)
+print(str + " and the world")
+
+print(face)
+
+print("potatos are yummy")
+print(face green); print(str)
